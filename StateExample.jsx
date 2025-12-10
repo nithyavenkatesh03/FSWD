bcefc9878304834309690fab4124a5f39765947a
nithyavenkatesh03 <nithyathiruvenkatesh@gmail.com>
2025-12-10 16:56:06 +0530

    Add files via upload

---
+++ StateExample.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+export default function StateExample()
+{
+    const[count,setCount]=useState(0);
+    function buttonClick() 
+    {
+        setCount(count+1);
+    }
+   return ( <>
+   <h1>State Example</h1>
+   <button onClick={buttonClick}>
+    visitor count={count}
+   </button>
+   </>);
+}
